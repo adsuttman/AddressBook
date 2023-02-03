a531a29a69c5548165bd74c33d9bbad80386fc7f
adsuttman <adsuttman@gmail.com>
2023-02-03 09:41:20 -0500

    initial commit + added Address class

---
+++ src/AddressBook.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+	List<Address> addresses = new ArrayList<Address>();
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
